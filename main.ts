32cde7f793f5c97e7c64d2f1863f315aa1c631d0
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2022-01-07 14:45:56 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+huskylens.initI2c()
+huskylens.initMode(protocolAlgorithm.ALGORITHM_FACE_RECOGNITION)
+basic.forever(function () {
+    huskylens.request()
+    if (huskylens.isLearned(1)) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            serial.writeValue("box", huskylens.readeBox(1, Content1.xCenter))
+            basic.setLedColor(0x00ff00)
+        } else {
+            basic.turnRgbLedOff()
+        }
+    }
+})
